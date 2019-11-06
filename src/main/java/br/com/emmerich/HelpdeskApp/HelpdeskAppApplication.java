649ee666de6f6d5aa515df23a313d85461635717
Thiago Emmerich <dev2@brametec.com.br>
2019-11-06 11:35:18 -0300

    Initializing the web project

---
+++ src/main/java/br/com/emmerich/HelpdeskApp/HelpdeskAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.emmerich.HelpdeskApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emmerich/HelpdeskApp/HelpdeskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emmerich.HelpdeskApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
